98f819b8c9d4d2593a24edf006dea4884853f492
BlackBear69420 <107828414+BlackBear69420@users.noreply.github.com>
2024-04-29 01:32:47 +0530

    auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,33 @@
 'use client'
 
 import Image from "next/image";
-import { SignInButton, SignOutButton, SignedIn, SignedOut, useSession } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, SignedIn, SignedOut, useOrganization, useSession, useUser } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
-   const files=useQuery(api.files.getFiles)
+  const organization=useOrganization();
+  const user =useUser();
+
+  let orgId=null;
+  if(organization.isLoaded && user.isLoaded){
+    orgId=organization.organization?.id ?? user.user?.id;
+  }
+   const files=useQuery(api.files.getFiles,orgId?{orgId}:"skip")
   const createFile=useMutation(api.files.createFile)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <SignedIn>
-      <SignOutButton>
-      <Button>Sign Out</Button>
-      </SignOutButton>
-    </SignedIn>
-    <SignedOut>
-    <SignInButton mode="modal">
-      <Button>Sign In</Button>
-    </SignInButton>
-    </SignedOut>
     {files?.map(file=>{
       return<div key={file._id}>
         {file.name}
       </div>
     })}
     <Button onClick={()=>{
+      if(!orgId) return;
       createFile({
-        name:'hello'
+        name:'hello',
+        orgId
       })
     }}>click me</Button>
    
